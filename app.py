5d72bbe023cd98d47ca7d8daa5042c854d7882ee
Brian S <tab126@gmail.com>
2025-12-28 03:56:21 +0000

    manual span added

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from random import randint
 from flask import Flask, request
 import logging
 
+from opentelemetry import trace
+
+# Acquire a tracer
+tracer = trace.get_tracer("diceroller.tracer")
+
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -10,12 +15,14 @@ logger = logging.getLogger(__name__)
 @app.route("/rolldice")
 def roll_dice():
     player = request.args.get('player', default=None, type=str)
-    result = str(roll())
-    if player:
-        logger.warning("%s is rolling the dice: %s", player, result)
-    else:
-        logger.warning("Anonymous player is rolling the dice: %s", result)
-    return result
+    with tracer.start_as_current_span("roll") as rollspan:
+        result = str(roll())
+        rollspan.set_attribute("roll.value", result)
+        if player:
+            logger.warning("%s is rolling the dice: %s", player, result)
+        else:
+            logger.warning("Anonymous player is rolling the dice: %s", result)
+        return result
 
 
 def roll():
