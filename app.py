4aeb996b6f7a495ab388eb8d4c4fffbddf843b75
Brian S <tab126@gmail.com>
2025-12-29 21:46:30 +0000

    sending to a container collector

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from random import randint
 from flask import Flask, request
 import logging
 
-from opentelemetry import trace
+from opentelemetry import trace, metrics
 
 # Acquire a tracer
 tracer = trace.get_tracer("diceroller.tracer")
+meter = metrics.get_meter("dicecontroller.meter")
+
+roll_counter = meter.create_counter(
+    "dice.rolls",
+    description="The number of rolls by value",
+)
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -14,10 +20,11 @@ logger = logging.getLogger(__name__)
 
 @app.route("/rolldice")
 def roll_dice():
-    player = request.args.get('player', default=None, type=str)
     with tracer.start_as_current_span("roll") as rollspan:
+        player = request.args.get('player', default=None, type=str)
         result = str(roll())
         rollspan.set_attribute("roll.value", result)
+        roll_counter.add(1, {'roll.value':result})
         if player:
             logger.warning("%s is rolling the dice: %s", player, result)
         else:
--- readme.md
+++ readme.md
@@ -9,3 +9,25 @@ opentelemetry-instrument \
     --logs_exporter console \
     --service_name dice-server \
     flask run -p 8080 --host 0.0.0.0
+
+export OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
+opentelemetry-instrument --logs_exporter otlp flask run -p 8080 --host 0.0.0.0
+
+
+
+## Collector
+podman run -p 4317:4317 \
+    -v `pwd`/otel-collector-config.yaml:/etc/otel-collector-config.yaml \
+    docker.io/otel/opentelemetry-collector:latest \
+    --config=/etc/otel-collector-config.yaml
+
+podman run --expose 55681 \
+    --expose 4317 \
+    --rm \
+    -v `pwd`/otel-collector-config.yaml:/etc/otel-collector-config.yaml \
+    -v `pwd`/../secrets/service-account-key.json:/etc/otelcol-contrib/key.json \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/etc/otelcol-contrib/key.json \
+    docker.io/otel/opentelemetry-collector-contrib:nightly-amd64 \
+    --config=/etc/otel-collector-config.yaml
+
+podman pull docker.io/otel/opentelemetry-collector-contrib:nightly-amd64
